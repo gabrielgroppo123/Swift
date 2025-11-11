d852f9ffeee953415e225853fad20f0f8a280869
Gabriel Groppo <gabgroppo@gmail.com>
2025-11-11 14:15:48 -0300

    Add files via upload

---
+++ Lesson1/Lesson1/main.swift
@@ -0,0 +1,138 @@
+import Foundation
+
+//print("Hello, World!");
+//print("Olá mundo")
+
+//var score = 123
+//print(score)
+
+//let num = 45
+//print(num)
+
+
+//let cidade: String = "São Paulo"
+//print(cidade)
+
+//let idade = 25
+//let nome = "Carlos"
+//print("Olá, \(nome), voce tem \(idade) anos")
+//print("Olá, "  + nome + ", voce tem " + String(idade) + " anos")
+
+//let idade1 = 24
+//let idade2 = 23
+
+//if(idade1 < idade2){
+    //print("Verdade")
+//}
+//else{
+    //print("Mentira")
+//}
+
+//var i = 0
+//while(i < 5){
+    //i+=1
+    //print(i, terminator: " ")
+//}
+//print("Digite uma frase: ")
+//let frase = readLine()
+//print(frase)
+
+//let inteiro = 10
+//let decimal = 10.2
+
+//let soma = Double(inteiro) + decimal
+//print(soma)
+
+//print("Digite seu nome: ", terminator: "")
+//let nome = readLine() ?? "Anonimo"
+//print("Olá, \(nome)!")
+
+//print("Digite uma frase:")
+
+//if let frase = readLine(){
+    //print(frase)
+//}else{
+    //print("Ué, pq nn digitou nada???")
+//}
+
+//print("Digite um numero:")
+//if let num = readLine(), let dobro = Int(num){
+    //print(dobro * 2)
+//}
+
+
+//print("Digite um ano:", terminator: "")
+//if let ano = readLine(), let anovalido = Int(ano){
+    //print("O cofre foi trancado no ano \(anovalido)")
+//}else{
+    //print("Entrada inválida! O cofre permanece selado por mais um século.")
+//}
+
+//print("Digite um numero inteiro:")
+//if let numero = readLine(), let numeroInt = Int(numero){
+    //print("Você usou \(numeroInt) ingredientes mágicos.")
+//}
+//else{
+    //print("Você usou \(1) ingrediente mágico.")
+//}
+
+
+//print("Digite uma senha:")
+//if let senha = readLine(){
+    //print("Nova senha definida: \(senha)")
+//}
+//else{
+    //print("Erro: nenhuma senha fornecida!")
+//}
+
+
+//print("Digite um numero:")
+//if let moeda = readLine(), let moedaInt = Int(moeda) {
+    //print("O poço aceitou sua oferenda: \(moedaInt)")
+//}
+//else{
+    //print("O poço ignorou palavras vazias...")
+//}
+
+//if let num = readLine(), let numeroInt = Int(num) {
+    //if numeroInt % 3 == 0{
+       // print("A porta se abriu!")
+    //}
+    //else{
+        //print("A porta continua trancada.")
+    //}
+//}
+//let total = 100
+//if let num = readLine(), let numeroInt = Int(num) {
+    //let resultado = total - numeroInt
+    //print("Você receberá \(resultado) moedas do tesouro!")
+//}
+
+//if let num1 = readLine(), let numeroInt1 = Int(num1) {
+    //if let num2 = readLine(), let numeroInt2 = Int(num2){
+        //let total = numeroInt1 + numeroInt2
+        //print(total)
+    //}
+//}
+
+//if let idade = readLine(), let idadeInt = Int(idade) {
+    //if let amuleto = readLine(), let amuletoTrue = Bool(amuleto){
+        //if let feiticeira = readLine(), let feiticeiraTrue = Bool(feiticeira){
+            //if idadeInt >= 18, amuletoTrue == true, feiticeiraTrue == true{
+              //  print("Reflexo revelado.")
+           // }
+            //else{
+               // print("Reflexo distorcido")
+            //}
+        //}
+    //}
+//}
+
+if let numero = readLine(), let numeroInt = Int(numero){
+    if numeroInt == 1{
+        print("O dragão aceitou sua joia solitária com respeito.")
+    }
+    else{
+        print("O dragão ficou decepcionado com a oferta de \(numeroInt) joias.")
+    }
+}
